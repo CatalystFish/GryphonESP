b507ce3e8d553f5881c474bad79ee704c4f84fd0
Fish!! <62106957+CatalystFish@users.noreply.github.com>
2024-06-26 11:53:43 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,33 +3,34 @@ const bodyParser = require('body-parser');
 const sqlite3 = require('sqlite3').verbose();
 
 const app = express();
-const db = new sqlite3.Database('./db/database.db');
+const db = new sqlite3.Database(':memory:');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname));
 
-// Serve the main page
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+// Initialize database
+db.serialize(() => {
+  db.run(`CREATE TABLE IF NOT EXISTS signins (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    firstName TEXT NOT NULL,
+    lastName TEXT NOT NULL,
+    company TEXT NOT NULL,
+    additionalData TEXT,
+    signInTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    signOutTime TIMESTAMP
+  )`);
 });
 
 // Handle sign-in form submission
 app.post('/signin', (req, res) => {
   const { username, password } = req.body;
   // Validate and authenticate user
-  // Redirect to post-sign-in page
   res.redirect('/post-sign-in');
-});
-
-// Serve post-sign-in page
-app.get('/post-sign-in', (req, res) => {
-  res.sendFile(__dirname + '/post-sign-in.html');
 });
 
 // Handle dynamic form submission
 app.post('/dynamic-sign-in', (req, res) => {
   const { firstName, lastName, company, additionalData } = req.body;
-  // Save data to database
   db.run(`INSERT INTO signins (firstName, lastName, company, additionalData) VALUES (?, ?, ?, ?)`, 
          [firstName, lastName, company, JSON.stringify(additionalData)], function(err) {
     if (err) {
@@ -42,7 +43,6 @@ app.post('/dynamic-sign-in', (req, res) => {
 // Handle sign-out
 app.post('/signout', (req, res) => {
   const { id } = req.body;
-  // Update sign-out time in database
   db.run(`UPDATE signins SET signOutTime = CURRENT_TIMESTAMP WHERE id = ?`, [id], function(err) {
     if (err) {
       return console.log(err.message);
@@ -55,3 +55,5 @@ app.post('/signout', (req, res) => {
 app.listen(3000, () => {
   console.log('Server running on http://localhost:3000');
 });
+
+module.exports = app;
